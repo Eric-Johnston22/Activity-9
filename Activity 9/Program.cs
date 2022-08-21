a5cc582793873f45017defa8ff56d4f859d8dc6a
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-08-21 17:30:45 -0400

    Add project files.

---
+++ Activity 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class myMethods
+{
+
+    // Method Stub 1
+    public void displaySum(int a, int b){}
+
+    // Method Stub 2
+    public double doubleAvg(double a, double b, double c, double d, double e){}
+
+    // Method Stub 3
+    public int randomSum(int a, int b){}
+
+    // Method Stub 4
+    public bool isDivisible(int a, int b, int c){}
+
+    // Method Stub 5
+    public void fewestChars(string a, string b){}
+
+    // Method Stub 6
+    public double largestVal(double[] a){}
+
+    // Method Stub 7
+    public int generateArr(int[] a){}
+
+    // Method Stub 8
+    public bool boolVal(bool a, bool b){}
+
+    // Method Stub 9
+    public double displayProduct(int a, double b){}
+
+    // Method Stub
+    public int arrAverage(int[,] a){}
+}
+
